e27172208b90b06bdff92a79439fc8433173de95
tuan-ng <iam.tuan.ng@gmail.com>
2019-09-14 19:04:54 -0400

    Updates

---
+++ precache-manifest.529c9f43aac13c008f60524a639614b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c39d326bf42b8496846b6d04965ce7",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "18d287096d23aac44a63",
+    "url": "/tic-tac-toe/static/css/main.54e57aa3.chunk.css"
+  },
+  {
+    "revision": "baf6187e459768c0271e",
+    "url": "/tic-tac-toe/static/js/2.e0e8de08.chunk.js"
+  },
+  {
+    "revision": "18d287096d23aac44a63",
+    "url": "/tic-tac-toe/static/js/main.173f8d9d.chunk.js"
+  },
+  {
+    "revision": "2447e331fa6994ee754e",
+    "url": "/tic-tac-toe/static/js/runtime~main.96c826cc.js"
+  }
+]);
